f333edd722237246900d5972b251d51ee4940149
Alon A. Rabinowitz <alonrab@outlook.com>
2024-12-23 19:39:02 +0200

    implement line tracking functionality with LED indication

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include <Arduino.h>
 
+#define LINE_TRACKER 17
+
+bool val = false;
+
 void setup()
 {
     Serial.begin(115200);
+    pinMode(LINE_TRACKER, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
+    val = digitalRead(LINE_TRACKER);
+    if (val == HIGH)
+    {
+        digitalWrite(LED_BUILTIN, LOW);
+        Serial.println("Line detected");
+    }
+    else
+    {
+        Serial.println("Line NOT detected");
+    }
+    delay(100);
 }
